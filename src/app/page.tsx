b3c34afe69eb9f292928eaa15996e8432a01bbc7
Neemias Santos <neemiassgc@outlook.com>
2025-01-05 02:22:48 -0300

    feat: make a first draft of the homepage sidebar with profile header, skills and social links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,55 @@
-import Image from "next/image";
+import { Avatar, Box, Chip } from "@mui/material"
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import Link from "next/link";
+import EmailIcon from '@mui/icons-material/Email';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const skills: string[] = [
+    "Java", "Spring", "Spring Boot", "JPA", "Hibernate", "Javascript", "Typescript", "TailwindCss", "NextJS",
+    "Spring Data JPA", "Kotlin", "Vercel", "Google Cloud", "AWS"
+  ]
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+  return (
+    <>
+      <Box className="w-full min-h-screen" component="div">
+        <Box component="div" className="h-full w-[360px] fixed top-0 left-0 z-50 pt-4 flex flex-col justify-between gap-0" sx={{borderRight: "1px solid grey"}}>
+          <Box>
+            <Avatar variant="circular" className="h-32 w-32 mx-auto" src="https://api.dicebear.com/9.x/fun-emoji/svg?seed=apples">N</Avatar>
+            <p className="text-lg text-center mt-3 font-mono">Neemias Santos</p>
+            <p className="text-3xl text-center mt-5 font-bold">Java Developer</p>
+            <p className="text-center mt-3 p-2">Com paixão por tecnologia, fica entusiasmado em criar coisas que torna a vida das pessoas mais fácil</p>
+          </Box>
+          <Box component="div" className="w-full pt-3 px-3 text-center">
+            <p className="text-2xl italic underline text-center mb-3">Skills</p>
+            {
+              skills.map((item, index) => <Chip label={item} className="m-1" key={index} />)
+            }
+          </Box>
+          <SidebarFooter/>
+        </Box>
+
+        <Box component="div" className="ml-[360px] w-full">
+
+        </Box>
+      </Box>
+    </>
+  )
+}
+
+function SidebarFooter() {
+  return (
+    <Box component="div" className="pb-1 text-center">
+      <Box component="div" className="mx-auto w-fit">
+        <Link href="https://linkedin.com/in/neemias-santos-68a348186">
+          <LinkedInIcon className="mr-3 text-4xl" sx={{color: "#0077B5"}}/>
+        </Link>
+        <Link href="https://github.com/neemiassgc">
+          <GitHubIcon className="text-4xl" sx={{color: "#181717"}}/>
+        </Link>
+        <Chip icon={<EmailIcon/>} label="neemiassgc@outlook.com" variant="outlined" className="ml-3"/>
+      </Box>
+      <p className="mt-4">Created by Neemias Santos</p>
+    </Box>
+  )
 }
