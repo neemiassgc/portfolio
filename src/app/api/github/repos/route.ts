b6e7d14927919d4ca2fd4344eb2a44be662394ef
Neemias Santos <neemiassgc@outlook.com>
2025-01-06 01:43:54 -0300

    feat(api): enhance repository data retrieval and also add one more step for parsing

--- src/app/api/github/repos/route.ts
+++ src/app/api/github/repos/route.ts
@@ -1,19 +1,41 @@
+import { GitHubRepository, RepositoryInfo } from "@/types";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  return NextResponse.json(await getPortfolioProjects());
+  const parsedRepositories = await parseRepositories(await getRepositories());
+  return NextResponse.json(parsedRepositories);
 }
 
-async function getPortfolioProjects(): Promise<string[]> {
-  const githubRepositories = await authorizedFetch();
-  return githubRepositories
-    .filter((repository: { topics?: string[] }) => repository?.topics?.includes("portfolio"))
-    .map((repository: { html_url: string }) => repository.html_url);
+async function parseRepositories(repositories: GitHubRepository[]): Promise<RepositoryInfo[]> {
+  const filteredRepositories: GitHubRepository[] = repositories
+    .filter((repository: GitHubRepository) => repository?.topics?.includes("portfolio"));
+    const mappedRepositories: Promise<RepositoryInfo>[] = filteredRepositories.map(async (repository: GitHubRepository) => {
+      const readme = await getReadme(repository);
+      return {
+        name: repository.name,
+        description: repository.description,
+        topics: repository.topics,
+        readme,
+        lastCommit: "26/12/24 on main",
+        url: repository.html_url
+      };
+    });
+
+    return Promise.all(mappedRepositories);
 }
 
-async function authorizedFetch(queryParams: { [key: string]: string } = {}) {
-  const url = new URL("https://api.github.com/user/repos");
-  Object.keys(queryParams).forEach(key => url.searchParams.append(key, queryParams[key]));
+async function getReadme(repository: GitHubRepository): Promise<string> {
+  const readme = await authorizedFetch(repository.contents_url.replace("{+path}", "README.md"));
+  return readme.content;
+}
+
+async function getRepositories() {
+  const githubRepositories = await authorizedFetch("https://api.github.com/user/repos");
+  return githubRepositories;
+}
+
+async function authorizedFetch(src: string) {
+  const url = new URL(src);
   const response =  await (fetch(url.toString(), {
     headers: {
       Accept: "application/vnd.github.v3+json",
