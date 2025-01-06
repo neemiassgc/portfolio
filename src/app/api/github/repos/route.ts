4698357a4e4dcdfb34632012d48e2c8125a22368
Neemias Santos <neemiassgc@outlook.com>
2025-01-05 22:05:21 -0300

    feat(api): add GET endpoint for portfolio projects

--- src/app/api/github/repos/route.ts
+++ src/app/api/github/repos/route.ts
@@ -1,3 +1,16 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json(await getPortfolioProjects());
+}
+
+async function getPortfolioProjects(): Promise<string[]> {
+  const githubRepositories = await authorizedFetch();
+  return githubRepositories
+    .filter((repository: { topics?: string[] }) => repository?.topics?.includes("portfolio"))
+    .map((repository: { html_url: string }) => repository.html_url);
+}
+
 async function authorizedFetch(queryParams: { [key: string]: string } = {}) {
   const url = new URL("https://api.github.com/user/repos");
   Object.keys(queryParams).forEach(key => url.searchParams.append(key, queryParams[key]));
