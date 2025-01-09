d1b0dcfeacaf222e1ad1ae337e6832d0b075b6fc
Neemias Santos <neemiassgc@outlook.com>
2025-01-09 00:20:09 -0300

    feat(tools): add function to tweak repositories by topics

--- src/app/tools.ts
+++ src/app/tools.ts
@@ -1,3 +1,18 @@
+import { RepositoryInfo } from "./types";
+
+export function tweakRepositoriesByTopics(repositories: RepositoryInfo[]): RepositoryInfo[][] {
+  const splittedRepositories: RepositoryInfo[][] = splitBy(repositories, 2);
+  const topicsLengthToRepositories: {[key: number]: RepositoryInfo[]} = splittedRepositories
+    .slice(0, splittedRepositories.length - 1)
+    .reduce((prev, repos) => {
+      const topicsSum: number = repos.reduce((acc, repo) => acc + repo.topics.length, 0);
+      return {...prev, [topicsSum]: repos};
+    }, {});
+  const topicsLength: number[] = Object.keys(topicsLengthToRepositories).map(Number);
+  topicsLengthToRepositories[Math.min(topicsLength[0], topicsLength[1])].push(...splittedRepositories[splittedRepositories.length - 1]);
+  return Object.values(topicsLengthToRepositories);
+}
+
 function splitBy<Type>(array: Type[], size: number): Type[][] {
   const result: Type[][] = [];
   let index: number = 0;
