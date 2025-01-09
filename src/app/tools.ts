0aa9a70d1eb05ce35ca11169649da48010258e7d
Neemias Santos <neemiassgc@outlook.com>
2025-01-09 00:07:18 -0300

    refactor(tools): correct array splitting logic

--- src/app/tools.ts
+++ src/app/tools.ts
@@ -6,7 +6,8 @@ export function splitBy<Type>(array: Type[], size: number): Type[][] {
     for(let j = 0; j < Math.floor(array.length / size); j++)
       result[i].push(array[index++]);
   }
+  result.push([]);
   for (let i = 0; i < array.length % size; i++)
-    result[i].push(array[index++]);
+    result[result.length - 1].push(array[index++]);
   return result;
 }
