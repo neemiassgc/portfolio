b3ed0c61bdf645d401b5442baa86c15ba73804fe
Neemias Santos <neemiassgc@outlook.com>
2025-01-13 12:03:09 -0300

    style(tools): format code and reorder array elements

--- src/app/tools.ts
+++ src/app/tools.ts
@@ -2,25 +2,30 @@ import { Repository } from "./types";
 
 export function tweakRepositoriesByTopics(repositories: Repository[]): Repository[][] {
   const splittedRepositories: Repository[][] = splitBy(repositories, 2);
+
   const topicsLengthToRepositories: {[key: number]: Repository[]} = splittedRepositories
     .slice(0, splittedRepositories.length - 1)
     .reduce((prev, repos) => {
       const topicsSum: number = repos.reduce((acc, repo) => acc + repo.topics.length, 0);
       return {...prev, [topicsSum]: repos};
     }, {});
+
   const topicsLength: number[] = Object.keys(topicsLengthToRepositories).map(Number);
   topicsLengthToRepositories[Math.min(topicsLength[0], topicsLength[1])].push(...splittedRepositories[splittedRepositories.length - 1]);
+
   return Object.values(topicsLengthToRepositories);
 }
 
 function splitBy<Type>(array: Type[], size: number): Type[][] {
   const result: Type[][] = [];
+  
   let index: number = 0;
   for (let i = 0; i < size; i++) {
     result.push([]);
     for(let j = 0; j < Math.floor(array.length / size); j++)
       result[i].push(array[index++]);
   }
+
   result.push([]);
   for (let i = 0; i < array.length % size; i++)
     result[result.length - 1].push(array[index++]);
@@ -37,16 +42,16 @@ export function beautifyRepositoryTitle(title: string): string {
 
 export function findTechStackNames(topics: string[]): [string, string, string] {
   const programmingLanguages: string[] = [
-    "java", "javascript", "typescript", "python",
-    "c#", "c++", "c", "kotlin", "ruby", "go", "rust"
+    "javascript", "typescript", "python", "c#",
+    "c++", "kotlin", "ruby", "go", "rust", "java"
   ]
   const frameworks: string[] = [
-    "spring-boot", "react", "angular", "vue", "nextjs", "gatsby",
+    "spring-boot", "angular", "vue", "nextjs", "gatsby", "react",
     "express", "flask", "django", "rails", "laravel", "symfony"
   ]
   const cloudPlatforms: string[] = [
     "aws", "google-cloud", "firebase", "github-pages",
-    "azure", "heroku", "netlify", "vercel", "digitalocean"
+    "azure", "heroku", "netlify", "vercel", "digital-ocean"
   ]
 
   return [
