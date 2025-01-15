55f3b606906a657b43d9011dff2172de0df1876b
Neemias Santos <neemiassgc@outlook.com>
2025-01-15 02:25:01 -0300

    feat(sidebar): enhance sidebar layout and content structure

--- src/app/components/sidebar.tsx
+++ src/app/components/sidebar.tsx
@@ -12,25 +12,27 @@ export default function Sidebar() {
   ]
   
   return (
-    <Box component="div" className="h-full w-[360px] fixed top-0 left-0 z-50 pt-4 flex flex-col justify-between gap-0">
-      <Box>
-        <Avatar variant="circular" className="h-32 w-32 mx-auto" src="https://api.dicebear.com/9.x/fun-emoji/svg?seed=apples">N</Avatar>
-        <Box component="p" className="text-lg text-center mt-1 font-mono" sx={{color: hues.text}}>Neemias Santos</Box>
-        <Box component="p" className="text-3xl text-center mt-1 font-bold" sx={{color: hues.text}}>Java Developer</Box>
-        <Box component="p" className="text-center mt-1 p-2">
-          Etiam imperdiet erat ac finibus porttitor. Nullam non dictum mauris. Phasellus ultrices bibendum sapien. Integer vehicula est vel risus congue, et efficitur eros tristique. Duis est augue, molestie ac enim ut, faucibus suscipit arcu. Maecenas odio justo, luctus sed viverra sed, facilisis ac nisl. Etiam iaculis nibh ac mollis imperdiet. Duis placerat tellus ex, ac varius libero ullamcorper vitae. Nulla tortor tortor, congue eu urna at, elementum iaculis dui.
+    <Box component="div" className="h-full w-[380px] fixed top-0 left-0">
+      <Box className="pt-4 flex flex-col justify-between gap-0 h-full pl-2">
+        <Box>
+          <Avatar variant="circular" className="h-32 w-32 mx-auto" src="https://api.dicebear.com/9.x/shapes/svg?seed=pine">N</Avatar>
+          <Box component="p" className="text-lg text-center mt-1 font-mono" sx={{color: hues.text}}>Neemias Santos</Box>
+          <Box component="p" className="text-3xl text-center mt-1 font-bold" sx={{color: hues.text}}>Java Developer</Box>
+          <Box component="p" className="text-center mt-1 p-2">
+            Etiam imperdiet erat ac finibus porttitor. Nullam non dictum mauris. Phasellus ultrices bibendum sapien. Integer vehicula est vel risus congue, et efficitur eros tristique. Duis est augue, molestie ac enim ut, faucibus suscipit arcu. Maecenas odio justo, luctus sed viverra sed, facilisis ac nisl. Etiam iaculis nibh ac mollis imperdiet. Duis placerat tellus ex, ac varius libero ullamcorper vitae. Nulla tortor tortor, congue eu urna at, elementum iaculis dui.
+          </Box>
         </Box>
+        <Box component="div" className="w-full pt-3 px-3 text-center">
+          <Box component="p" className="text-2xl font-bold hover:underline text-center mb-3" sx={{color: hues.text}}>Skills</Box>
+          {
+            skills.map((item, index) =>
+              <Chip
+                sx={{backgroundColor: hues['bg-secondary'], color: hues['text-secondary']}}
+                size="small" label={item} className="m-1" key={index} />)
+          }
+        </Box>
+        <SidebarFooter/>
       </Box>
-      <Box component="div" className="w-full pt-3 px-3 text-center">
-        <Box component="p" className="text-2xl italic hover:underline text-center mb-3" sx={{color: hues.text}}>Skills</Box>
-        {
-          skills.map((item, index) =>
-            <Chip
-              sx={{backgroundColor: hues['bg-secondary'], color: hues['text-secondary']}}
-              size="small" label={item} className="m-1" key={index} />)
-        }
-      </Box>
-      <SidebarFooter/>
     </Box>
   )
 }
@@ -52,7 +54,7 @@ function SidebarFooter() {
           icon={<EmailIcon/>} label="neemiassgc@outlook.com" variant="filled" className="ml-3"
         />
       </Box>
-      <Box component="p" className="mt-4">{"Made with </> by Neemias Santos"}</Box>
+      <Box component="p">{"Made with </> by "}{<Box component="span" className="font-bold">Neemias Santos</Box>}</Box>
     </Box>
   )
 }
