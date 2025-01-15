b084a731f4cf6a03761737736568d4691c19375c
Neemias Santos <neemiassgc@outlook.com>
2025-01-15 01:22:09 -0300

    refactor(net): simplify authorizedFetch function parameters

--- src/app/net.ts
+++ src/app/net.ts
@@ -32,14 +32,13 @@ async function getDocsLink(gitHubRepository: GitHubRepository): Promise<string |
   }
 }
 
-async function authorizedFetch(src: string) {
-  const url = new URL(src);
-  const response =  await (fetch(url.toString(), {
+async function authorizedFetch(url: string) {
+  const response =  await fetch(url, {
     headers: {
       Accept: "application/vnd.github.v3+json",
       Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
       "X-GitHub-Api-Version": "2022-11-28"
     }
-  }));
+  });
   return response.json();
 }
