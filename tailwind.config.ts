95d5119e0c69bf4c1895c2011292b953050a786c
Neemias Santos <neemiassgc@outlook.com>
2025-08-27 01:55:28 -0300

    feat: introduces Subframe UI with Taildwind CSS

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,22 +5,9 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src\ui/**/*.{tsx,ts,js,jsx}"
   ],
-  theme: {
-    fontFamily: {
-      "core": ["var(--font-core)"],
-    },
-    extend: {
-      backgroundColor: {
-        "hues-primary": "#fffffe",
-        "hues-secondary": "#f2f4f6",
-      },
-      textColor: {
-        "hues-primary": "#00214d",
-        "hues-secondary": "#1b2d45",
-        "hues-highlight": "#ff5470"
-      },
-    }
-  },
+  theme: {},
   plugins: [],
+  presets: [require("./src\\ui/tailwind.config.js")]
 } satisfies Config;
