28dd062e43930b0c6a51e8e96ef8b7e50d1cfd0d
Neemias Santos <neemiassgc@outlook.com>
2025-08-30 02:33:33 -0300

    style: Format tailwind config and globals.css

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,7 +5,7 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src\ui/**/*.{tsx,ts,js,jsx}"
+    "./src/ui/**/*.{tsx,ts,js,jsx}"
   ],
   theme: {},
   plugins: [],
