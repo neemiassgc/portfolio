22327e07459d3c3cdfe80a56d5e3e0bde7f9e3d4
Neemias Santos <neemiassgc@outlook.com>
2025-09-08 02:21:42 -0300

    feat: Enable turbopack and update Next.js version

--- next.config.ts
+++ next.config.ts
@@ -1,29 +1,10 @@
-import type { NextConfig } from "next";
+import path from 'path';
 
-const nextConfig: NextConfig = {
-  webpack(config) {
-    const fileLoaderRule = config.module.rules.find((rule: { test?: { test?: (v: string) => void } }) =>
-      rule.test?.test?.('.svg'),
-    )
-
-    config.module.rules.push(
-      {
-        ...fileLoaderRule,
-        test: /\.svg$/i,
-        resourceQuery: /url/,
-      },
-      {
-        test: /\.svg$/i,
-        issuer: fileLoaderRule.issuer,
-        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
-        use: ['@svgr/webpack'],
-      },
-    )
-
-    fileLoaderRule.exclude = /\.svg$/i
-
-    return config
+module.exports = {
+  turbopack: {
+    root: path.join(__dirname, '..'),
   },
+
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [
@@ -36,5 +17,3 @@ const nextConfig: NextConfig = {
     ]
   },
 }
-
-export default nextConfig;
