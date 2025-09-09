56c95dfa28b7e8a844fb9ce8d74b9bd62de792cf
Neemias Santos <neemiassgc@outlook.com>
2025-09-08 21:21:13 -0300

    feat(ProjectNavBar): Add toggle group to navigate to project sections

--- src/app/components/ProjectNavBar.tsx
+++ src/app/components/ProjectNavBar.tsx
@@ -1,15 +1,27 @@
 "use client"
 
 import { ToggleGroup } from "@/ui/components/ToggleGroup"
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function ProjectNavBar() {
+  const [value, setValue] = useState("");
+  const router = useRouter();
+  const goTo = (value: string) => {
+    const valueToIdMap: { [key: string]: string } = {
+      "8614a054": "backend",
+      "2e913acf": "frontend",
+      "5d2544b1": "game",
+      "d8e536a7": "tool"
+    }
+    setValue(value);
+    router.push(`#${valueToIdMap[value]}`);
+  }
+
   return (
     <div className="flex w-full items-center gap-4">
       <div className="flex w-full items-center gap-4">
-        <ToggleGroup value="" onValueChange={(value: string) => {}}>
-          <ToggleGroup.Item icon="FeatherGrid" value="bf4918ae">
-            All
-          </ToggleGroup.Item>
+        <ToggleGroup value={value} onValueChange={goTo}>
           <ToggleGroup.Item icon="FeatherServer" value="8614a054">
             Backend
           </ToggleGroup.Item>
