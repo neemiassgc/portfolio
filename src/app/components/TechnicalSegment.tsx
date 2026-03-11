f250ed3a26782cfdd7be8d8460fdc4aa26b40c34
Neemias Santos <neemiassgc@outlook.com>
2026-03-11 19:17:22 -0300

    refactor: create component to show tech badges through shields.io

--- src/app/components/TechnicalSegment.tsx
+++ src/app/components/TechnicalSegment.tsx
@@ -1,8 +1,8 @@
 import { Variant } from "@/types";
-import { Badge } from "@/ui/components/Badge";
 import { IconWithBackground } from "@/ui/components/IconWithBackground";
 import { IconName } from "@subframe/core";
 import Header from "./Header";
+import Image from "next/image";
 
 export default function TechnicalSegment() {
   return (
@@ -31,7 +31,7 @@ export default function TechnicalSegment() {
           }}
           badges={{
             iconName: "FeatherCode",
-            items: ["HTML5", "CSS3", "JavaScript", "React", "TypeScript", "Next.js", "Tailwind CSS", "Subframe", "Vercel", "Auth0"]
+            items: ["HTML5", "CSS", "JavaScript", "React", "TypeScript", "Next.js", "Tailwind CSS", "Subframe", "Vercel", "Auth0"]
           }}
         />
         <BadgeSet
@@ -86,15 +86,56 @@ function BadgeSet(props: {
       </div>
       <div className="flex flex-wrap items-start gap-2">
         {
-          props.badges.items.map((item, index) => {
+          props.badges.items.map(e => e.toLowerCase()).map((item, index) => {
+            const convertedItem = item === "java" ? "openjdk" : item.includes("google") ? "google cloud" : item;
             return (
-              <Badge variant="neutral" icon={props.badges.iconName} key={index}>
-                {item}
-              </Badge>
+              <Shield key={index} title={item} logo={convertedItem} logoColor={badgesToColorMap[convertedItem]}/>
             )
           })
         }
       </div>
     </div>
   )
+}
+
+async function Shield(props: { title: string, logo: string, logoColor: string}) {
+  return (
+    <Image
+      alt={props.title}
+      width={0}
+      height={0}
+      className="w-fit h-fit"
+      src={`https://img.shields.io/badge/${props.title}-f1f5f9?logo=${
+        props.logo.toLowerCase()}&logoColor=${props.logoColor}&style=for-the-badge`}
+    />
+  )
+}
+
+const badgesToColorMap: { [key: string]: string } = {
+  "html": "E34F26",
+  "css": "663399",
+  "javascript": "F7DF1E",
+  "react": "61DAFB",
+  "typescript": "3178C6",
+  "next.js": "000000",
+  "tailwind css": "06B6D4",
+  "openjdk": "000000",
+  "subframe": "000000",
+  "vercel": "000000",
+  "auth0": "EB5424",
+  "spring": "6DB33F",
+  "spring boot": "6DB33F",
+  "spring security": "6DB33F",
+  "hibernate": "59666C",
+  "docker": "2496ED",
+  "git": "F05032",
+  "intellij idea": "000000",
+  "postgresSQL": "4169E1",
+  "firebase": "DD2C00",
+  "supabase": "3FCF8E",
+  "linux": "FCC624",
+  "insomnia": "4000BF",
+  "notion": "000000",
+  "json": "4000BF",
+  "google cloud": "4285F4",
 }
