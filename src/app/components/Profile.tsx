ee94de696b57627903891c3c511e818880662850
Neemias <neemiassgc@outlook.com>
2026-05-31 02:10:57 -0300

    refactor(profile): rename Hero to Profile and load dynamic data

--- src/app/components/Profile.tsx
+++ src/app/components/Profile.tsx
@@ -4,17 +4,23 @@ import SubframeCore, { IconName } from "@subframe/core";
 import Header from "./Header";
 import Image from "next/image";
 import Link from "next/link";
+import { findContactData, findSectionData } from "@/integration/notion";
+import { Lang } from "@/types";
 
-export default function Hero() {
+export default async function Profile(props: { lang: Lang }) {
+  const contactData = await findContactData();
+  const profileSectionData = await findSectionData(0, props.lang);
+
   return (
     <div className="flex flex-col md:flex-row justify-center lg:justify-between max-w-[1280px] flex-wrap items-center" id="home">
       <div className="flex grow shrink-0 basis-0 flex-col items-start gap-8">
-        <Header sectionName="PORTFOLIO" title="Hi, I'm Neemias Santos">
-          Full-Stack Web Developer turning ideas into digital experiences with usage of modern technologies.
+        <Header sectionName="PORTFOLIO" title={profileSectionData.title} subtitle={profileSectionData.subtitle}>
+          { profileSectionData.content }
         </Header>
         <div className="flex flex-col items-start gap-4">
-          <AboutMe iconName="FeatherMail" text="neemiassgc@outlook.com" />
-          <AboutMe iconName="FeatherMapPin" text="São Paulo, BR" />
+          <AboutMe iconName="FeatherMail" text={contactData.email} />
+          <AboutMe iconName="FeatherMapPin" text={contactData.location} />
+          <AboutMe iconName="FeatherPhone" text={contactData.phone} />
         </div>
         <div className="flex flex-wrap items-center gap-4 justify-center sm:justify-between">
           <Link href="#connect">
@@ -33,22 +39,20 @@ export default function Hero() {
             Download Resume
           </Button>
           <div className="flex items-center gap-2">
-            <IconButton
-              icon="FeatherLinkedin"
-
-            />
-            <IconButton
-              icon="FeatherGithub"
-
-            />
+            <Link href={"https://www.linkedin.com/in/neemiassgc/"}>
+              <IconButton icon="FeatherLinkedin"/>
+            </Link>
+            <Link href={"https://github.com/neemiassgc"}>
+              <IconButton icon="FeatherGithub"/>
+            </Link>
           </div>
         </div>
       </div>
       <div className="order-first md:order-none h-96 w-96 flex items-center overflow-hidden rounded-full bg-brand-100">
         <Image
-          width={112}
-          height={112}
-          src="https://api.dicebear.com/9.x/adventurer/svg?seed=Mary"
+          width={448}
+          height={448}
+          src={contactData.profilePhoto}
           alt="avatar"
           className="h-112 w-112 flex-none object-cover"
         />
