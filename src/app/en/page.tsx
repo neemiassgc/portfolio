193a7f959dc7c2ec2a483cba65fc000ed1293cc8
Neemias <neemiassgc@outlook.com>
2026-05-31 02:20:35 -0300

    feat: add English landing page and refactor main page to use LandingPage

---
+++ src/app/en/page.tsx
@@ -0,0 +1,7 @@
+import LandingPage from "@/components/LandingPage";
+
+function Page() {
+  return <LandingPage lang="en"/>;
+}
+
+export default Page;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,7 @@
-import React from "react";
-import { DefaultPageLayout } from "@/ui/layouts/DefaultPageLayout";
-import Hero from "./components/Hero";
-import TechnicalSegment from "./components/TechnicalSegment";
-import ProjectSegment from "./components/ProjectSegment";
-import ConnectSegment from "./components/ConnectSegment";
+import LandingPage from "./components/LandingPage"
 
-function LandingPage() {
-  return (
-    <DefaultPageLayout>
-      <div className="flex h-full w-full flex-col items-start overflow-auto">
-        <div className="flex w-full flex-col items-center gap-16 px-4 py-16">
-          <Hero/>
-          <TechnicalSegment/>
-          <ProjectSegment/>
-          <ConnectSegment/>
-        </div>
-        <div className="flex w-full flex-col items-center border-t border-solid border-neutral-border bg-neutral-50 px-4 py-4">
-          <span className="text-body font-body text-subtext-color">
-            © 2024 All rights reserved. Built with passion and modern
-            technologies
-          </span>
-        </div>
-      </div>
-    </DefaultPageLayout>
-  );
+function Page() {
+  return <LandingPage lang="pt-br"/>;
 }
 
-export default LandingPage;
+export default Page;
