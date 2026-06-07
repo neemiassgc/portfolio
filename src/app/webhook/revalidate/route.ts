f8dccca35fba4ce7ac68826eb26529feac38afeb
Neemias <neemiassgc@outlook.com>
2026-06-07 12:32:00 -0300

    feat(revalidate): add Notion signature support and rename route file

--- src/app/webhook/revalidate/route.ts
+++ src/app/webhook/revalidate/route.ts
@@ -4,7 +4,8 @@ import { NextRequest, NextResponse } from "next/server";
 import  { createHmac } from "node:crypto";
 
 export async function POST(request: NextRequest) {
-  const isValid = await validateRequest(request);
+  const isValid = await validateRequest(request, "X-Hub-Signature-256" )
+    || await validateRequest(request, "X-Notion-Signature");
 
   if (!isValid) return new NextResponse(null, { status: 403 });
 
@@ -12,18 +13,21 @@ export async function POST(request: NextRequest) {
   return new NextResponse("Ok", { status: 200 });
 }
 
-async function validateRequest(request: NextRequest): Promise<boolean> {
+async function validateRequest(request: NextRequest, signatureHeader: string): Promise<boolean> {
   const headerList = await headers();
-  const signature = headerList.get("X-Hub-Signature-256");
+  const signature = headerList.get(signatureHeader);
+  
   if (!signature) return false;
 
   const body = await request.text();
-  const sha256 = generateSha(body);
+  const sha256 = generateSha(body, signatureHeader);
   return sha256 === signature.slice(7);
 }
 
-function generateSha(input: string): string {
-  const secret = process.env.GITHUB_SECRET as string;
+function generateSha(input: string, signatureHeader: string): string {
+  const secret = signatureHeader === "X-Hub-Signature-256" ?
+    process.env.GITHUB_SECRET as string :
+    process.env.NOTION_SECRET as string;
   const hash = createHmac('sha256', secret)
     .update(input)
     .digest('hex');
