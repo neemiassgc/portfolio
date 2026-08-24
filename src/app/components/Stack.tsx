827804baa9728a913e8db81d3ebf6b7cb9a025b1
Neemias <neemiassgc@outlook.com>
2026-08-24 01:22:02 -0300

    feat (components): add Stack component to render tech icons

---
+++ src/app/components/Stack.tsx
@@ -0,0 +1,21 @@
+import { fetchRawIcon, TechIconNames } from "@/misc";
+import Image from "next/image";
+
+export default async function Stack(props: { names: TechIconNames[] }) {
+  return (
+    <div className="flex gap-4">
+      {
+        await Promise.all(props.names.map(async (name, key) => {
+          const rawIcon = await fetchRawIcon(name);
+          return <Image
+            key={key}
+            width={32}
+            height={32}
+            src={`data:image/svg+xml;base64,${Buffer.from(rawIcon).toString("base64")}`}
+            alt={`${name} icon`}
+          />
+        }))
+      }
+    </div>
+  );
+}
